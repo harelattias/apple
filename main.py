c2174ea7edf85ebc49f667d924752732c78d0f80
git <git@mail.com>
2022-02-11 09:20:58 +0200

    random

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pgzrun
-
+import random
 
 def draw():
-    screen.fill((0, 0, 0))
+    screen.fill((0, 170, 0))
     apple.draw()
 
 
 def place_apple():
-    apple.x = 300
-    apple.y = 200
+    apple.x = random.randint(1, 800)
+    apple.y = random.randint(1, 540)
 
 
 apple = Actor("apple")
