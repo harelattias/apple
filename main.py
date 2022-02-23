54eab9b20cb294a2232436dfe82170408716132b
git <git@mail.com>
2022-02-23 17:01:46 +0200

    end

--- main.py
+++ main.py
@@ -2,36 +2,35 @@ import pgzrun
 import random
 import pygame
 
-
 def on_mouse_down(pos):
     global number_good_shots
+    global number_of_attempts
+
+    number_of_attempts = number_of_attempts +1
+    if number_of_attempts ==  11:
+        quit()
+
     if apple.collidepoint(pos):
         number_good_shots = number_good_shots + 1
-        print("good shot!", number_good_shots)
         place_apple()
-    else:
-        print("you missed!")
+
 
 def draw():
     screen.fill((0, 170, 0))
     apple.draw()
     global number_good_shots
-    pygame.display.set_caption("Number of good shots : " + str(number_good_shots))
+    global number_of_attempts
+    #pygame.display.set_caption("Number of good shots : " + str(number_good_shots))
+    screen.draw.text("number of hits -> " + str(number_good_shots) + " / " + str(number_of_attempts), (20, 20), owidth=1, ocolor="blue")
 
-
-# def draw_number_of_hits():
-#     global number_good_shots
-#     font = pygame.font.SysFont(None, 48)
-#     img = font.render(number_good_shots, True, RED)
-#     screen.draw.text("hello world", (20, 20), owidth=1, ocolor="blue")
 
 def place_apple():
     apple.x = random.randint(1, 800)
     apple.y = random.randint(1, 540)
 
 
-global number_good_shots
 number_good_shots = 0
+number_of_attempts = 0
 apple = Actor("apple")
 place_apple()
 
